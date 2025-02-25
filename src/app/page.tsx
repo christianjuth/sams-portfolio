58749779246bd8a2de1a96b03db0be94ef1cb058
Christian <5768801+christianjuth@users.noreply.github.com>
2025-02-24 19:19:28 -0500

    setup basic press kit layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,139 @@
-import Image from "next/image";
+import { DetailedHTMLProps, InputHTMLAttributes, TextareaHTMLAttributes } from "react"
+import { FaFacebook, FaInstagram, FaSpotify } from "react-icons/fa";
+import 'react-slideshow-image/dist/styles.css'
+import { Images } from "./images";
+import Image from "next/image"
 
-export default function Home() {
+function YouTubeSection({ videoId }: { videoId: string }) {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <iframe className="w-full aspect-video"
+      src={`https://www.youtube.com/embed/${videoId}`} />
+  )
+}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+function Input(props: DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>) {
+  return (
+    <input {...props} className={`p-2 text-black ${props.className}`} />
+  )
+}
+
+function TextArea(props: DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>) {
+  return (
+    <textarea {...props} className={`p-2 text-black ${props.className}`} />
+  )
+}
+
+function Button({ children, href, type, ...props }: {
+  children: React.ReactNode,
+  href?: string
+  type?: "submit"
+  className?: string
+}) {
+  const className = `border border-white p-4 text-center ${props.className}`
+
+  if (href) {
+    return (
+      <a href={href} className={className}>
+        {children}
+      </a>
+    )
+  }
+
+  return (
+    <button className={className} type={type}>{children}</button>
+  )
+}
+
+function Section({ children, className }: { children: React.ReactNode, className?: string }) {
+  return (
+    <section className={`${className} relative max-w-7xl w-full mx-auto mb-32 px-6`}>
+      {children}
+    </section>
+  )
+}
+
+export default function Page() {
+  return (
+    <>
+      <div className="flex flex-col relative">
+        <div className="sticky top-0 h-[100vh]">
+          <Image src="/hero.png" fill alt="" className="object-cover object-left" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+
+        <Section className="-mt-[100vh] mb-0">
+          <div className="flex flex-col gap-14 md:items-center justify-center min-h-[100svh] mx-auto relative">
+            <div className="flex flex-row gap-4 text-4xl">
+              <FaFacebook />
+              <FaInstagram />
+              <FaSpotify />
+            </div>
+
+            <h1 className="text-6xl font-black">SAMANTHA DRESS</h1>
+
+            <p className="text-lg">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+
+            <Button href="mailto:hi@samanthadress.com" className="w-full">Book Now</Button>
+          </div>
+        </Section>
+
+        <Section>
+          <YouTubeSection videoId="-k0XST5khaA" />
+        </Section>
+
+        <Section>
+          <section className="grid md:grid-cols-2 gap-8">
+            <div className="bg-zinc-50 aspect-square relative">
+              <Image src="/about.png" fill alt="" className="object-cover object-left" />
+            </div>
+
+            <div className="flex flex-col gap-4 justify-center bg-black/45 p-6 backdrop-blur-md leading-7">
+              <h2 className="font-bold text-3xl">Samantha Dress</h2>
+
+              <p>
+                Lorem ipsum odor amet, consectetuer adipiscing elit. Condimentum bibendum iaculis commodo himenaeos egestas habitant cras proin. Mi aenean quis in ornare faucibus elementum pellentesque. Sollicitudin dui dui primis feugiat scelerisque vivamus praesent torquent? Ornare tellus sed, primis eros faucibus urna. Eu id feugiat aliquet ultricies amet libero.
+              </p>
+              <p>
+                Lorem ipsum odor amet, consectetuer adipiscing elit. Condimentum bibendum iaculis commodo himenaeos egestas habitant cras proin. Mi aenean quis in ornare faucibus elementum pellentesque. Sollicitudin dui dui primis feugiat scelerisque vivamus praesent torquent? Ornare tellus sed, primis eros faucibus urna. Eu id feugiat aliquet ultricies amet libero.
+              </p>
+              <p>
+                Lorem ipsum odor amet, consectetuer adipiscing elit. Condimentum bibendum iaculis commodo himenaeos egestas habitant cras proin. Mi aenean quis in ornare faucibus elementum pellentesque. Sollicitudin dui dui primis feugiat scelerisque vivamus praesent torquent? Ornare tellus sed, primis eros faucibus urna. Eu id feugiat aliquet ultricies amet libero.
+              </p>
+            </div>
+          </section>
+        </Section>
+
+        <Section>
+          <YouTubeSection videoId="-k0XST5khaA" />
+        </Section>
+
+        <Section>
+          <Images />
+        </Section>
+      </div>
+
+      <div className="bg-black py-32 relative">
+        <Section className="gap-4 flex flex-col bg-black">
+          <h2 className="text-3xl font-black">BOOKING</h2>
+
+          <form action="https://formsubmit.co/hi@samanhadress.com" method="POST" className="flex flex-col gap-1">
+            <label>Name</label>
+            <Input type="text" name="name" required />
+
+            <br />
+            <label>Email</label>
+            <Input type="email" name="email" required />
+
+            <br />
+            <label>Message</label>
+            <TextArea name="message" placeholder="Book Sammantha" />
+
+            <br />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Section>
+      </div>
+    </>
+  )
 }
