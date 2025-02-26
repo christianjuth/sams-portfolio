88d15edef00ea350b9b1919380463a8f85429c69
Christian <5768801+christianjuth@users.noreply.github.com>
2025-02-25 19:15:19 -0500

    fix favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" type="image/png" href="/favicon.png" />
-      </head>
       <body
         className={`${poppins.variable} antialiased bg-black text-white`}
       >
