52688c7dbbc11d7ec81db7ae639456a48b5d9f52
Christian <5768801+christianjuth@users.noreply.github.com>
2025-07-20 11:10:33 -0400

    refactor: change audio embed to spotify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,48 @@
-import { twMerge } from 'tailwind-merge'
-import Image from 'next/image'
+import { twMerge } from "tailwind-merge";
+import Image from "next/image";
 
-import TheRoomies from '@/../public/the-roomies.jpeg'
+import TheRoomies from "@/../public/the-roomies.jpeg";
 
 import { ImageSlider } from "@/components/sections/images";
-import { AudioPlayer } from "@/components/sections/audio-player";
 import { BackgroundImage } from "@/components/sections/background-image";
 import { YouTubeSection } from "@/components/sections/youtube-embed";
 import { Hero } from "@/components/sections/hero";
 import { ContactForm } from "@/components/sections/contact-form";
 import { Bio } from "@/components/sections/bio";
 
-import Link from 'next/link';
-import { ExternalLink } from '@/components/ui';
-import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa';
+import Link from "next/link";
+import { ExternalLink } from "@/components/ui";
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
 
-function Section({ children, className }: { children: React.ReactNode, className?: string }) {
+function SpotifyEmbed() {
   return (
-    <section className={twMerge("relative max-w-7xl w-full mx-auto mb-20 md:mb-32 px-6", className)}>
+    <iframe
+      src="https://open.spotify.com/embed/track/1AwVF0lSMwlru3CE1K3cqQ?utm_source=generator"
+      className="h-64 w-full"
+      allowFullScreen={false}
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      loading="lazy"
+    />
+  );
+}
+
+function Section({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <section
+      className={twMerge(
+        "relative max-w-7xl w-full mx-auto mb-20 md:mb-32 px-6",
+        className,
+      )}
+    >
       {children}
     </section>
-  )
+  );
 }
 
 export default function Page() {
@@ -29,7 +51,6 @@ export default function Page() {
       <BackgroundImage src="/hero.png" />
 
       <div className="flex flex-col relative">
-
         <Section className="mb-0 md:mb-0">
           <Hero
             title="SAMANTHA DRESS"
@@ -56,35 +77,20 @@ export default function Page() {
         </Section>
 
         <Section>
-          <AudioPlayer playlist={[
-            {
-              src:
-                "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3",
-              title: "Song 1",
-              artist: "Samantha Dress"
-            },
-            {
-              src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3",
-              title: "Song 2",
-              artist: "Samantha Dress"
-            },
-            {
-              src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3",
-              title: "Song 3",
-              artist: "Samantha Dress"
-            }
-          ]} />
+          <SpotifyEmbed />
         </Section>
 
         <Section>
-          <ImageSlider images={[
-            "/slide-3.png",
-            "/slide-4.png",
-            "/slide-1.png",
-            "/slide-2.png",
-            "/slide-5.png",
-            "/slide-6.png",
-          ]} />
+          <ImageSlider
+            images={[
+              "/slide-3.png",
+              "/slide-4.png",
+              "/slide-1.png",
+              "/slide-2.png",
+              "/slide-5.png",
+              "/slide-6.png",
+            ]}
+          />
           <Link href="/photos" className="text-center block pt-3">
             View all photos
           </Link>
@@ -97,22 +103,31 @@ export default function Page() {
         </Section>
       </div>
 
-      <div className="bg-white backdrop-blur-xl py-20 relative text-black" id="book">
+      <div
+        className="bg-white backdrop-blur-xl py-20 relative text-black"
+        id="book"
+      >
         <Section className="mb-0 md:mb-0 flex flex-col max-md:items-center">
           <h2 className="text-xl block mb-6">Other projects</h2>
 
           <div className="max-w-xs flex flex-col items-center gap-2">
             <Image src={TheRoomies} alt="The Roomies" />
             <div className="flex flex-row gap-2 items-center text-2xl">
-              <ExternalLink href="https://www.youtube.com/@theroomies387/featured"><FaYoutube /></ExternalLink>
+              <ExternalLink href="https://www.youtube.com/@theroomies387/featured">
+                <FaYoutube />
+              </ExternalLink>
               |
-              <ExternalLink href="https://www.instagram.com/theroomiesduo"><FaInstagram /></ExternalLink>
+              <ExternalLink href="https://www.instagram.com/theroomiesduo">
+                <FaInstagram />
+              </ExternalLink>
               |
-              <ExternalLink href="https://www.facebook.com/theroomiesduo"><FaFacebook /></ExternalLink>
+              <ExternalLink href="https://www.facebook.com/theroomiesduo">
+                <FaFacebook />
+              </ExternalLink>
             </div>
           </div>
         </Section>
       </div>
     </>
-  )
+  );
 }
--- src/components/sections/audio-player.tsx
+++ src/components/sections/audio-player.tsx
@@ -1,24 +1,42 @@
-"use client"
+"use client";
 
-import { useRef, useState } from 'react';
-import H5AudioPlayer from 'react-h5-audio-player';
-import 'react-h5-audio-player/lib/styles.css';
-import { FaSpotify, FaPlay, FaPause } from 'react-icons/fa';
+import { useRef, useState } from "react";
+import H5AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+import { FaSpotify, FaPlay, FaPause } from "react-icons/fa";
 
 type Song = {
   src: string;
   title: string;
   artist: string;
-}
+};
 
-export function AudioPlayer({
-  playlist
-}: {
-  playlist: Song[]
-}) {
-  const ref = useRef<H5AudioPlayer>(null)
+/**
+ * @example
+ * <AudioPlayer
+ *   playlist={[
+ *     {
+ *       src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3",
+ *       title: "Song 1",
+ *       artist: "Samantha Dress",
+ *     },
+ *     {
+ *       src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3",
+ *       title: "Song 2",
+ *       artist: "Samantha Dress",
+ *     },
+ *     {
+ *       src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3",
+ *       title: "Song 3",
+ *       artist: "Samantha Dress",
+ *     },
+ *   ]}
+ * />
+ */
+export function AudioPlayer({ playlist }: { playlist: Song[] }) {
+  const ref = useRef<H5AudioPlayer>(null);
 
-  const [playing, setPlaying] = useState(false)
+  const [playing, setPlaying] = useState(false);
 
   const [index, setIndex] = useState<number | null>(null);
   const projectedIndex = index ?? 0;
@@ -27,25 +45,24 @@ export function AudioPlayer({
     if (projectedIndex < playlist.length - 1) {
       setIndex(projectedIndex + 1);
       requestAnimationFrame(() => {
-        ref.current?.audio.current?.play()
-      })
+        ref.current?.audio.current?.play();
+      });
     }
-  }
+  };
 
   const handlePrev = () => {
     if (projectedIndex > 0) {
       setIndex(projectedIndex - 1);
       requestAnimationFrame(() => {
-        ref.current?.audio.current?.play()
-      })
+        ref.current?.audio.current?.play();
+      });
     }
-  }
+  };
 
   return (
     <div className="flex flex-col bg-neutral-700">
-
       <div className="grid md:grid-rows-2 grid-cols-2 p-6 bg-neutral-500 max-md:gap-4">
-        <div className="md:row-span-2" >
+        <div className="md:row-span-2">
           <div className="aspect-square bg-black w-44 rounded-md" />
         </div>
         <div className="flex flex-row justify-end p-2 text-2xl">
@@ -59,9 +76,9 @@ export function AudioPlayer({
           onClickPrevious={handlePrev}
           onPlay={() => {
             if (!index) {
-              setIndex(0)
+              setIndex(0);
             }
-            setPlaying(true)
+            setPlaying(true);
           }}
           onPause={() => setPlaying(false)}
           showSkipControls
@@ -75,39 +92,33 @@ export function AudioPlayer({
       </div>
 
       <div className="py-2 flex flex-col flex-1">
-        {playlist.map(({ src, title, artist }, i) =>
+        {playlist.map(({ src, title, artist }, i) => (
           <button
             key={src}
             onClick={() => {
               if (index === i && playing) {
                 requestAnimationFrame(() => {
-                  ref.current?.audio.current?.pause()
-                })
+                  ref.current?.audio.current?.pause();
+                });
               } else {
                 setIndex(i);
                 requestAnimationFrame(() => {
-                  ref.current?.audio.current?.play()
-                })
+                  ref.current?.audio.current?.play();
+                });
               }
             }}
             className="flex flex-row items-center py-2 px-4 gap-4 hover:bg-black/50 transition-colors"
           >
             <span className="text-neutral-400 w-5">
-              {index === i ? (playing ? <FaPause /> : <FaPlay />) : <>{i + 1}</>}
+              {index === i ? playing ? <FaPause /> : <FaPlay /> : <>{i + 1}</>}
             </span>
             <div className="flex flex-col text-start">
-              <span>
-                {title}
-              </span>
-              <span className="text-sm text-neutral-300">
-                {artist}
-              </span>
+              <span>{title}</span>
+              <span className="text-sm text-neutral-300">{artist}</span>
             </div>
           </button>
-        )}
+        ))}
       </div>
-
-
     </div>
-  )
+  );
 }
