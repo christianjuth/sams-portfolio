37373fdff5de2cce77846596011396db5f246e7f
Christian <5768801+christianjuth@users.noreply.github.com>
2025-07-24 17:17:50 -0400

    refactor: improve event cal data fetching

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,36 @@
 import type { Metadata } from "next";
 import { Playfair_Display } from "next/font/google";
+import { TanstackQueryProvider } from "@/components/tanstack-query";
 import "./globals.css";
 
 const poppins = Playfair_Display({
   variable: "--font-playfair-display",
   weight: "800",
-  subsets: ['latin']
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
   title: "Samantha Dress",
-  description: "singer • songwriter • instrumentalist • performer • producer • music educator",
+  description:
+    "singer • songwriter • instrumentalist • performer • producer • music educator",
   openGraph: {
     type: "website",
-    locale: 'en_US',
+    locale: "en_US",
     siteName: "Samantha Dress",
     images: [
       {
-        url: 'https://samanthadress.com/hero.png',
+        url: "https://samanthadress.com/hero.png",
         width: 1800,
         height: 1600,
-        alt: 'Samantha Dress',
+        alt: "Samantha Dress",
       },
-    ]
+    ],
   },
   twitter: {
-    card: 'summary_large_image',
+    card: "summary_large_image",
     images: {
-      url: 'https://samanthadress.com/hero.png',
-      alt: 'Samantha Dress',
+      url: "https://samanthadress.com/hero.png",
+      alt: "Samantha Dress",
     },
   },
 };
@@ -40,10 +42,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${poppins.variable} antialiased bg-black text-white`}
-      >
-        {children}
+      <body className={`${poppins.variable} antialiased bg-black text-white`}>
+        <TanstackQueryProvider>{children}</TanstackQueryProvider>
       </body>
     </html>
   );
--- src/components/sections/tour-calendar.tsx
+++ src/components/sections/tour-calendar.tsx
@@ -3,13 +3,48 @@
 import dayjs from "dayjs";
 import localizedFormat from "dayjs/plugin/localizedFormat";
 import ICAL from "ical.js";
-import { useEffect, useState } from "react";
+import { useMemo, useState } from "react";
 import { Button } from "../ui";
 import _ from "lodash";
 import { cn } from "@/lib/cn";
 import getUrls from "get-urls";
+import { useQuery } from "@tanstack/react-query";
+import z from "zod";
+import { ImSpinner8 } from "react-icons/im";
 
 dayjs.extend(localizedFormat);
+
+const whateveroriginSchema = z.object({
+  contents: z.string(),
+  status: z.object({
+    http_code: z.number(),
+  }),
+});
+
+const icalTextSchema = z.string().refine(
+  (text) => {
+    const trimmed = text.trim();
+
+    // must start/end correctly
+    if (
+      !trimmed.startsWith("BEGIN:VCALENDAR") ||
+      !trimmed.endsWith("END:VCALENDAR")
+    ) {
+      return false;
+    }
+
+    // must include at least one VERSION: and one PRODID:
+    if (!/VERSION:\d+(\.\d+)?/.test(trimmed) || !/PRODID:.+/.test(trimmed)) {
+      return false;
+    }
+
+    return true;
+  },
+  {
+    message:
+      "Invalid iCal format: must start with BEGIN:VCALENDAR, end with END:VCALENDAR, and include VERSION & PRODID",
+  },
+);
 
 function proxyFetch(url: string) {
   return Promise.any([
@@ -17,23 +52,33 @@ function proxyFetch(url: string) {
       const res = await fetch(
         `https://thingproxy.freeboard.io/fetch/${encodeURIComponent(url)}`,
       );
-      return await res.text();
+      const text = await res.text();
+      return icalTextSchema.parse(text);
     })(),
     (async () => {
       const res = await fetch(
         `https://api.allorigins.win/raw?url=${encodeURIComponent(url)}`,
       );
-      return await res.text();
+      const text = await res.text();
+      return icalTextSchema.parse(text);
     })(),
     (async () => {
       const res = await fetch(`https://whateverorigin.org/get?url=${url}`);
       const json = await res.json();
-      if (_.isObject(json) && "contents" in json && _.isString(json.contents)) {
-        return json.contents;
+      const { status, contents } = whateveroriginSchema.parse(json);
+      if (status.http_code < 200 || status.http_code >= 300) {
+        throw new Error("server error");
       }
-      throw new Error("failed to parse text response");
+      return icalTextSchema.parse(contents);
     })(),
   ]);
+}
+
+function useCalendar(icalUrl: string) {
+  return useQuery({
+    queryKey: ["ical", icalUrl],
+    queryFn: () => proxyFetch(icalUrl),
+  });
 }
 
 const today = dayjs();
@@ -48,36 +93,46 @@ export function TourCalendar({
   maxEvents?: number;
 }) {
   const [maxEvents, setMaxEvents] = useState(maxEventsProp ?? 9999);
-  const [events, setEvents] = useState<ICAL.Event[]>([]);
+
+  const query = useCalendar(icalUrl);
+  const events: ICAL.Event[] = useMemo(() => {
+    const icsText = query.data;
+
+    if (!icsText) {
+      return [];
+    }
+
+    const jcalData = ICAL.parse(icsText);
+    const comp = new ICAL.Component(jcalData);
+    const vevents = comp.getAllSubcomponents("vevent");
+
+    return vevents
+      .map((vevent) => new ICAL.Event(vevent))
+      .filter((event) => {
+        const startDate = dayjs(event.startDate.toJSDate());
+        return !startDate.isBefore(today, "day");
+      })
+      .sort((a, b) =>
+        a.startDate.toString().localeCompare(b.startDate.toString()),
+      );
+  }, [query.data]);
 
   const hiddenEvents = events.length - maxEvents;
-
-  useEffect(() => {
-    async function loadCal() {
-      const icsText = await proxyFetch(icalUrl);
-      const jcalData = ICAL.parse(icsText);
-      const comp = new ICAL.Component(jcalData);
-      const vevents = comp.getAllSubcomponents("vevent");
-      setEvents(
-        vevents
-          .map((vevent) => new ICAL.Event(vevent))
-          .filter((event) => {
-            const startDate = dayjs(event.startDate.toJSDate());
-            return !startDate.isBefore(today, "day");
-          })
-          .sort((a, b) =>
-            a.startDate.toString().localeCompare(b.startDate.toString()),
-          ),
-      );
-    }
-    loadCal();
-  }, [icalUrl]);
 
   return (
     <div className="bg-black p-6">
       <h2 className="font-bold text-3xl mb-4">{title}</h2>
 
-      <div className="divide-y divide-white/20">
+      <div
+        className={cn(
+          "divide-y divide-white/20 relative",
+          query.isPending && "min-h-96",
+        )}
+      >
+        {query.isPending && (
+          <ImSpinner8 className="absolute top-1/2 left-1/2 text-3xl animate-spin" />
+        )}
+
         {events.slice(0, maxEvents).map((event) => {
           const startDate = dayjs(event.startDate.toJSDate());
           const endDate = dayjs(event.endDate.toJSDate());
