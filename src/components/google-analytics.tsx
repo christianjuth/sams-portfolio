e09e3a2744ab7f2d37490870b0d27075e43113ae
Christian <5768801+christianjuth@users.noreply.github.com>
2025-08-20 19:38:23 -0400

    refactor: update analytics and clean url on load

--- src/components/google-analytics.tsx
+++ src/components/google-analytics.tsx
@@ -1,14 +1,28 @@
+import Script from 'next/script';
+
 export function GoogleAnalytics({ gaId }: { gaId: string }) {
   return (
     <>
-      <script async src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}></script>
-      <script>
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${gaId}')`}
-      </script>
+      <Script
+        id="ga-src"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga-init" strategy="afterInteractive">
+        {`window.dataLayer=window.dataLayer||[];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config','${gaId}');`}
+      </Script>
+
+      <Script id="utm-clean" strategy="afterInteractive">
+        {`(function(){
+              var url=new URL(location.href), p=new URLSearchParams(url.search),
+                  keys=['utm_source','utm_medium','utm_campaign','utm_term','utm_content','gclid','fbclid','msclkid','twclid','yclid','dm_i'];
+              var changed=false; keys.forEach(k=>{ if(p.has(k)){p.delete(k); changed=true;}});
+              if(changed){ url.search=p.toString(); history.replaceState(history.state,'',url.toString()); }
+            })();`}
+      </Script>
     </>
   )
 }
