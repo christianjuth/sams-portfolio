9235b43b28048b7695e020c0109c5fd892704d18
Christian <5768801+christianjuth@users.noreply.github.com>
2025-09-01 16:47:21 -0400

    refactor: update facebook link

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   instagram: "https://www.instagram.com/_samanthadress",
-  facebook: "https://www.facebook.com/sam.laubach.music/",
+  facebook: "https://www.facebook.com/samanthadressmusic",
   spotify: "https://open.spotify.com/artist/0Zuzwx8CeIvrDwqgC6k043",
   appleMusic: "https://music.apple.com/us/artist/samantha-dress/1823365334",
   jobDescription: "singer • songwriter • instrumentalist • performer • producer • music educator",
